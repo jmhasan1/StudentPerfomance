d9dce1aa0c018b02ee8fe6db816545efa53eaa60
jmhasan1 <jmhasan17@gmail.com>
2024-04-09 00:36:41 +0530

    updates

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# automatically find out all the poackages in the Machine Learning application (directory)that we have created
+# find packages - automatically find out all the poackages in the Machine Learning application (directory)that we have created
 from setuptools import find_packages , setup
 from typing import List
 
@@ -12,8 +12,8 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     # opening requirements as a temporary file
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        # lis comprehension below to replace \n with blanks at the end of each line
+        requirements = file_obj.readlines() 
+        # list comprehension below to replace \n with blanks at the end of each line
         requirements=[req.replace("\n"," ")for req in requirements]
 
         if HYPHEN_E_DOT in requirements:
@@ -21,7 +21,8 @@ def get_requirements(file_path:str)->List[str]:
 
     return requirements
 
-# metadata about the project
+# metadata about the project 
+# doing the setup
 setup(
     name='StudentPerfomance',
     version='0.0.1',
@@ -30,6 +31,8 @@ setup(
     packages=find_packages(),
     install_requires =get_requirements('requirements.txt')
 )
-# whenever find packages is running it will look for folders with __init__ as package as build this
-# entire project development will happen inside source folder
+# whenever find packages is running it will look for folders with __init__ as package as build this  (will directly cosider src as a package itself ; then builds this)
+# entire project development will happen inside src folder
 # install_requires will automatically install all the required libraries
+
+# -e . in requirements.txt automatically triggers setup.py file whenever we try to install requirements
